94be7b420acfb03260f0fee41bda8f5978c294a5
CareyWYR <wyr95626@163.com>
2024-05-20 22:58:48 +0800

    init

---
+++ api/glm.py
@@ -0,0 +1,17 @@
+from zhipuai import ZhipuAI
+import os
+
+
+glm_key = os.environ.get('glm_key')
+client = ZhipuAI(api_key=glm_key)
+response = client.chat.completions.create(
+    model="glm-4",  # 填写需要调用的模型名称
+    messages=[
+        {"role": "user", "content": "作为一名营销专家，请为我的产品创作一个吸引人的slogan"},
+        {"role": "assistant", "content": "当然，为了创作一个吸引人的slogan，请告诉我一些关于您产品的信息"},
+        {"role": "user", "content": "智谱AI开放平台"},
+        {"role": "assistant", "content": "智启未来，谱绘无限一智谱AI，让创新触手可及!"},
+        {"role": "user", "content": "创造一个更精准、吸引人的slogan"}
+    ],
+)
+print(response.choices[0].message)
