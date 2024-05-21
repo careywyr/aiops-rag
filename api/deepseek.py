bfe30a0d90addc0305538312cec0bbe368061cd3
CareyWYR <wyr95626@163.com>
2024-05-21 21:01:04 +0800

    init

---
+++ api/deepseek.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+import os
+
+deepseek_key = os.environ.get('deepseek_key')
+
+client = OpenAI(api_key=deepseek_key, base_url="https://api.deepseek.com")
+
+
+def chat(message: str) -> str:
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant"},
+            {"role": "user", "content": "Hello"},
+        ],
+        stream=False
+    )
+
+    return response.choices[0].message.content
